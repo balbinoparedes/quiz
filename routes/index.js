1382df4dd959ee0c49fa537a69775e45ac529852
Balbino Paredes Casas <b.paredes@outlook.com>
2015-08-16 20:06:23 +0200

    Autenticacion, Sesion y Autorizacion

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 // opcion controlador para creditos autor :
 // var authorController = require('../controllers/author_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* Página de entrada (home page) */
 router.get('/', function(req, res, next) {
@@ -13,15 +14,23 @@ router.get('/', function(req, res, next) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizid
+
+//Definición de las rutas de session
+router.get('/login', sessionController.new);      //Cargar Formulario de login
+router.post('/login', sessionController.create);  //Crear la sesión de usuario
+router.get('/logout', sessionController.destroy); //Destruir la sesión. DELETE
+
 //Definición de la route /quizes
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',          quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
+//Definición de route /quizes para la edicion de preguntas
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
 //Definición de la route comments en /quizes
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
